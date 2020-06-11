1afbc61d50cb26871c63d3cba03fd25a54be15f4
Renato <renatomarquesteles@gmail.com>
2020-06-11 15:30:42 -0300

    resolucao do desafio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { formatDate } from '../utils/FormatDate';
 
 class Contact extends React.Component {
   render() {
-    return (<div />);
+    return (
+      <article className="contact" data-testid="contact">
+        <span className="contact__avatar">
+          <img src={this.props.data.avatar} alt="avatar" />
+        </span>
+        <span className="contact__data" data-testid="contact-name">
+          {this.props.data.name}
+        </span>
+        <span className="contact__data" data-testid="contact-phone">
+          {this.props.data.phone}
+        </span>
+        <span className="contact__data" data-testid="contact-country">
+          {this.props.data.country}
+        </span>
+        <span className="contact__data" data-testid="contact-date">
+          {formatDate(this.props.data.admissionDate)}
+        </span>
+        <span className="contact__data" data-testid="contact-company">
+          {this.props.data.company}
+        </span>
+        <span className="contact__data" data-testid="contact-department">
+          {this.props.data.department}
+        </span>
+      </article>
+    );
   }
 }
 
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,97 @@
 import React from 'react';
 
 class Filters extends React.Component {
-	render() {
-		return (<div />);
-	}
+  constructor(props) {
+    super(props);
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleFilterButtonChange = this.handleFilterButtonChange.bind(this);
+  }
+
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleFilterButtonChange(e) {
+    this.props.onFilterButtonChange(e.target.name);
+  }
+
+  render() {
+    const sortBy = this.props.sortBy;
+    return (
+      <div className="container" data-testid="filters">
+        <section className="filters">
+          <div className="filters__search">
+            <input
+              type="text"
+              className="filters__search__input"
+              placeholder="Pesquisar"
+              onChange={this.handleFilterTextChange}
+              value={this.props.filterText}
+            />
+
+            <button className="filters__search__icon">
+              <i className="fa fa-search" />
+            </button>
+          </div>
+
+          <button
+            className={`filters__item ${
+              sortBy === 'name' ? 'is-selected' : ''
+            }`}
+            type="button"
+            name="name"
+            onClick={this.handleFilterButtonChange}
+          >
+            Nome
+          </button>
+
+          <button
+            className={`filters__item ${
+              sortBy === 'country' ? 'is-selected' : ''
+            }`}
+            type="button"
+            name="country"
+            onClick={this.handleFilterButtonChange}
+          >
+            País
+          </button>
+
+          <button
+            className={`filters__item ${
+              sortBy === 'company' ? 'is-selected' : ''
+            }`}
+            type="button"
+            name="company"
+            onClick={this.handleFilterButtonChange}
+          >
+            Empresa
+          </button>
+
+          <button
+            className={`filters__item ${
+              sortBy === 'department' ? 'is-selected' : ''
+            }`}
+            type="button"
+            name="department"
+            onClick={this.handleFilterButtonChange}
+          >
+            Departamento
+          </button>
+
+          <button
+            className={`filters__item ${
+              sortBy === 'admissionDate' ? 'is-selected' : ''
+            }`}
+            type="button"
+            name="admissionDate"
+            onClick={this.handleFilterButtonChange}
+          >
+            Data de admissão
+          </button>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Filters;
